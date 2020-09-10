e689d0e26fcff3226ebd6fdb876d4405f318e09c
limagoHub <limagoHub@limago.de>
2020-09-10 10:04:28 +0200

    Initial Commit

---
+++ UebungModulA/src/de/client/Main.java
@@ -0,0 +1,24 @@
+package de.client;
+
+
+
+import java.lang.reflect.Method;
+
+import de.gothaer.b.InterfaceB;
+import de.gothaer.c.InterfaceC;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		InterfaceB.getInstance().print();
+
+		InterfaceC.getInstance().print();
+		
+		Class<?> clazz = Class.forName("de.gothaer.b.impl.ServiceB");
+		Object o = clazz.newInstance();
+		Method m = clazz.getDeclaredMethod("printPrivate");
+		m.setAccessible(true);
+		m.invoke(o);
+	}
+
+}
---
+++ UebungModulA/src/module-info.java
@@ -0,0 +1,3 @@
+module modulA {
+	requires modulB;
+}
